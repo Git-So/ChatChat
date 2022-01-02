35016953a9545a59163ee6fbf05aef675c7e38e9
So <im.lfq@139.com>
2022-01-03 00:59:42 +0800

    feat: 添加 CahtBar

---
+++ src/renderer/widget/ChatBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <n-layout-header bordered class="chat-bar">
+        <n-grid>
+            <n-grid-item span="4" class="n-gi-btn">
+                <n-button text>
+                    <n-icon size="25">
+                        <menu-filled />
+                    </n-icon>
+                </n-button>
+            </n-grid-item>
+            <n-grid-item span="20">
+                <n-input placeholder="搜索" />
+            </n-grid-item>
+        </n-grid>
+    </n-layout-header>
+</template>
+
+<script lang="ts" setup>
+import { NButton, NIcon, NInput, NLayoutHeader, NGrid, NGridItem } from "naive-ui";
+import { MenuFilled } from "@vicons/material";
+</script>
+
+<style lang="scss" scoped>
+.chat-bar {
+    padding: 5px 8px;
+
+    .n-gi-btn {
+        display: flex;
+    }
+}
+</style>
