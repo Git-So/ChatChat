95a4dc130488938985b2a31d20ddf1f305f8bcc9
So <im.lfq@139.com>
2022-01-08 23:10:05 +0800

    feat: 完善账号列表组件

---
+++ src/data/Account.ts
@@ -0,0 +1,5 @@
+export interface Account {
+  id: number;
+  avatar: string;
+  nickname: string;
+}
--- src/renderer/widget/DrawerAccount.vue
+++ src/renderer/widget/DrawerAccount.vue
@@ -2,14 +2,24 @@
     <n-collapse-transition :show="isShow">
         <n-layout-header bordered>
             <n-list>
+                <n-list-item v-for="account in accounts">
+                    <template #prefix>
+                        <n-avatar round :size="32" :src="account.avatar" />
+                    </template>
+                    <div>{{ account.nickname }}</div>
+                    <template #suffix v-if="selectedAccountId == account.id">
+                        <n-icon size="20" color="blue">
+                            <check-filled />
+                        </n-icon>
+                    </template>
+                </n-list-item>
                 <n-list-item>
                     <template #prefix>
-                        <n-icon>
+                        <n-icon size="32" class="icon">
                             <account-circle-filled />
                         </n-icon>
                     </template>
                     <div>添加账号</div>
-                    <template #suffix></template>
                 </n-list-item>
             </n-list>
         </n-layout-header>
@@ -18,13 +28,23 @@
 
 <script lang="ts" setup>
 import { NLayoutHeader, NList, NListItem, NAvatar, NCollapseTransition, NIcon, } from "naive-ui";
-import { AccountCircleFilled } from "@vicons/material"
+import { AccountCircleFilled, CheckFilled, } from "@vicons/material"
+import { Account } from "../../data/Account";
 
 defineProps({
     isShow: Boolean,
+    accounts: Array as () => Array<Account>,
+    selectedAccountId: Number,
 })
 
 </script>
 
 <style lang="scss" scoped>
+.icon {
+    display: flex;
+}
+
+:deep(.n-list-item) {
+    border-bottom: 0 !important;
+}
 </style>
