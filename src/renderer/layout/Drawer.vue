a69b939df56a74627dc82a4e7c8e736c8c1883de
So <im.lfq@139.com>
2022-01-08 23:37:18 +0800

    fix: 修复侧边栏滑动使用原生组件问题

--- src/renderer/layout/Drawer.vue
+++ src/renderer/layout/Drawer.vue
@@ -7,11 +7,19 @@
     >
         <n-drawer-content class="content">
             <template #header>
-                <DrawerHeader @handle="onChangeDrawerAccount" />
+                <DrawerHeader
+                    :account="account"
+                    @handle="onChangeDrawerAccount"
+                    :up="isShowDrawerAccount"
+                />
             </template>
-            <n-layout>
-                <DrawerAccount :show="isShowDrawerAccount" />
-                <Menu :menu="menu" />
+            <n-layout :native-scrollbar="false" class="drawer-option">
+                <DrawerAccount
+                    :show="isShowDrawerAccount"
+                    :accounts="accounts"
+                    :selected-account-id="selectedAccountId"
+                />
+                <Menu :menu="menu" hide-border />
             </n-layout>
             <template #footer>
                 <n-space vertical>
@@ -24,7 +32,7 @@
 </template>
 
 <script lang="ts" setup>
-import { NDrawer, NDrawerContent, NSpace, NLayout, NLayoutHeader, } from "naive-ui";
+import { NDrawer, NDrawerContent, NSpace, NLayout, } from "naive-ui";
 import { computed, ref } from "vue";
 import Menu from "../widget/Menu.vue";
 import DrawerHeader from "../widget/DrawerHeader.vue";
@@ -33,7 +41,30 @@ import store from "../store";
 import { ExposeDrawer } from "./interface/Expose";
 import { PersonFilled, GroupRound, SettingsFilled, DarkModeFilled, } from "@vicons/material";
 import { Menu as IMenu, MenuType } from "../type";
+import { Account as IAccount } from "../../data/Account";
 
+const account: IAccount = {
+    id: 1,
+    uid: "305784840",
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=305784840&s=100",
+    nickname: "这是昵称",
+}
+    ;
+
+const selectedAccountId = 2
+const accounts: Array<IAccount> = [{
+    id: 1,
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=305784840&s=100",
+    nickname: "这是昵称",
+}, {
+    id: 2,
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=305782840&s=100",
+    nickname: "一个昵称",
+}, {
+    id: 3,
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=3057848&s=100",
+    nickname: "还是昵称",
+}]
 
 const menu: Array<IMenu> = [{
     icon: PersonFilled,
@@ -45,15 +76,31 @@ const menu: Array<IMenu> = [{
 }, {
     icon: PersonFilled,
     title: "联系人",
-    description: "我的联系人，哈哈哈哈哈",
+    description: "我的联系人，哈哈哈哈哈1111",
 }, {
     icon: PersonFilled,
     type: MenuType.Switch,
     title: "联系人",
-    description: "我的联系人，哈哈哈哈哈",
+    description: "我的联系人，哈哈哈哈哈1111",
 }, {
     icon: SettingsFilled,
     title: "设置",
+}, {
+    icon: DarkModeFilled,
+    title: "夜间模式",
+    type: MenuType.Switch,
+}, {
+    icon: DarkModeFilled,
+    title: "夜间模式",
+    type: MenuType.Switch,
+}, {
+    icon: DarkModeFilled,
+    title: "夜间模式",
+    type: MenuType.Switch,
+}, {
+    icon: DarkModeFilled,
+    title: "夜间模式",
+    type: MenuType.Switch,
 }, {
     icon: DarkModeFilled,
     title: "夜间模式",
@@ -68,9 +115,14 @@ const onChangeDrawerAccount = () => {
 
 // 显示状态
 const isShow = computed(() => store.state.isShowDrawer)
-const onUpdateShowState = (isShow: boolean) => store.commit("updateDrawerState", isShow)
+const onUpdateShowState = (isShow: boolean) => {
+    store.commit("updateDrawerState", isShow)
+    if (!isShow) isShowDrawerAccount.value = false
+}
 const onShow = () => onUpdateShowState(true)
 const onHide = () => onUpdateShowState(false)
+
+
 
 defineExpose(<ExposeDrawer>{
     onUpdateShowState, onShow, onHide,
@@ -78,9 +130,29 @@ defineExpose(<ExposeDrawer>{
 </script>
 
 <style lang="scss" scoped>
+@import "../style/common.scss";
+
 .content {
+    :deep(.n-list) {
+        padding: 8px 16px;
+    }
+
+    :deep(.n-drawer-header) {
+        padding-bottom: 4px;
+    }
+
+    :deep(.n-drawer-body),
+    :deep(.n-drawer-body-content-wrapper) {
+        @extend %flex-column;
+        padding: 0;
+    }
+
     :deep(.n-drawer-header__main) {
         width: 100%;
+    }
+
+    :deep(.n-drawer-body .n-drawer-body-content-wrapper) {
+        padding-top: 0;
     }
 }
 </style>
