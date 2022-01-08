a01296ac6e699c6efa34379a3fbb303750236012
So <im.lfq@139.com>
2022-01-08 23:08:03 +0800

    refactor: 菜单布局重构

--- src/renderer/widget/Menu.vue
+++ src/renderer/widget/Menu.vue
@@ -1,31 +1,25 @@
 <template>
     <n-list class="menu">
         <n-list-item v-for="item in menu" class="item" :class="{ 'hide-border': hideBorder }">
-            <n-grid>
-                <n-grid-item span="4" class="icon">
-                    <n-icon size="32">
-                        <component :is="item.icon"></component>
-                    </n-icon>
-                </n-grid-item>
-                <n-grid-item span="20" class="desc">
-                    <div class="content">
-                        <div class="info">
-                            <div class="title">{{ item.title }}</div>
-                            <div v-if="item.description" class="description">{{ item.description }}</div>
-                        </div>
-                        <div class="extra" v-if="item.type">
-                            <n-switch />
-                        </div>
-                    </div>
-                </n-grid-item>
-            </n-grid>
+            <template #prefix>
+                <n-icon size="32">
+                    <component :is="item.icon"></component>
+                </n-icon>
+            </template>
+            <div class="content">
+                <div class="title">{{ item.title }}</div>
+                <div v-if="item.description" class="description">{{ item.description }}</div>
+            </div>
+            <template #suffix>
+                <n-switch />
+            </template>
         </n-list-item>
     </n-list>
 </template>
 
 
 <script lang="ts" setup>
-import { NList, NListItem, NGrid, NGridItem, NIcon, NSwitch, } from "naive-ui";
+import { NList, NListItem, NIcon, NSwitch, } from "naive-ui";
 import { Menu } from "../type";
 
 defineProps({
@@ -35,49 +29,28 @@ defineProps({
 </script>
 
 <style lang="scss" scoped>
+@import "../style/common.scss";
+
 .menu {
-    margin-block-start: 0;
-    margin-block-end: 0;
-
     .item {
-        padding: 6px 0;
-
         &.hide-border {
             border: none;
         }
     }
-
-    .icon {
-        display: flex;
-    }
-
+    
     .content {
         display: flex;
-        flex-direction: row;
-        width: 100%;
+        flex-direction: column;
+        min-width: 0;
 
-        .info {
-            width: 100%;
-
-            .title,
-            .description {
-                overflow: hidden;
-                text-overflow: ellipsis;
-                white-space: nowrap;
-            }
-
-            .title {
-                font-weight: 500;
-            }
-
-            .description {
-                color: #999;
-                font-size: 13px;
-            }
+        .title,
+        .description {
+            @extend %text-ellipsis;
         }
 
-        .extra {
-            flex-shrink: 0;
+        .description {
+            color: #999;
+            font-size: 13px;
         }
     }
 }
