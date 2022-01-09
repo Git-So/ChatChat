980a8c41a1d5d942add9efe61f8956846ba5cc03
So <im.lfq@139.com>
2022-01-09 23:21:05 +0800

    refactor: 列表使用菜单替代

--- src/renderer/page/setting/Index.vue
+++ src/renderer/page/setting/Index.vue
@@ -3,20 +3,7 @@
         <Titlebar title="设置"></Titlebar>
         <n-layout has-sider class="main">
             <n-layout-sider class="sider" bordered>
-                <n-list>
-                    <template v-for="item in routes" key="item.name">
-                        <router-link :to="item.name" custom replace v-slot="{ navigate }">
-                            <n-list-item @click="navigate">
-                                <template #prefix>
-                                    <n-icon size="42">
-                                        <component :is="item.icon"></component>
-                                    </n-icon>
-                                </template>
-                                <n-thing :title="item.title" :description="item.description"></n-thing>
-                            </n-list-item>
-                        </router-link>
-                    </template>
-                </n-list>
+                <Menu :menu="menus" />
             </n-layout-sider>
             <n-layout>
                 <router-view />
@@ -26,44 +13,41 @@
 </template>
 
 <script lang="ts" setup>
-import { NLayout, NLayoutSider, NList, NListItem, NIcon, NThing } from "naive-ui";
+import { NLayout, NLayoutSider, } from "naive-ui";
 import { SettingsApplicationsFilled, ExtensionFilled, DeveloperModeFilled, InfoFilled } from "@vicons/material";
 import Titlebar from "../../widget/Titlebar.vue"
-import { Ref, ref, shallowRef } from "vue";
+import Menu from "../../widget/Menu.vue"
+import { MenuRouterLink as MenuRouterLink } from "../../type";
 
+const menus: Array<MenuRouterLink> = [{
+    route: "common",
+    replace: true,
+    icon: SettingsApplicationsFilled,
+    title: "通用",
+    description: "软件通用功能配置",
+},
+{
+    route: "plugin",
+    replace: true,
+    icon: ExtensionFilled,
+    title: "插件",
+    description: "软件插件配置",
+},
+{
+    route: "developer",
+    replace: true,
+    icon: DeveloperModeFilled,
+    title: "开发者选项",
+    description: "开发相关配置",
+},
+{
+    route: "about",
+    replace: true,
+    icon: InfoFilled,
+    title: "关于",
+    description: "软件信息与说明",
+},];
 
-interface SettingRoute {
-    name: string;
-    icon: Ref;
-    title: string;
-    description: string;
-}
-const routes = ref<SettingRoute[]>([
-    {
-        name: "common",
-        icon: shallowRef(SettingsApplicationsFilled),
-        title: "通用",
-        description: "软件通用功能配置",
-    },
-    {
-        name: "plugin",
-        icon: shallowRef(ExtensionFilled),
-        title: "插件",
-        description: "软件插件配置",
-    },
-    {
-        name: "developer",
-        icon: shallowRef(DeveloperModeFilled),
-        title: "开发者选项",
-        description: "开发相关配置",
-    },
-    {
-        name: "about",
-        icon: shallowRef(InfoFilled),
-        title: "关于",
-        description: "软件信息与说明",
-    },
-]);
 </script>
 
 
